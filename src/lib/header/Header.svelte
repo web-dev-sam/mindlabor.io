6458c7574cbff5778c9e21cf80fb27e54500baf1
Samuel Braun <42311986+MindLaborDev@users.noreply.github.com>
2022-01-15 17:56:01 +0100

    Fixed changed svelte api

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -6,17 +6,17 @@
 <header>
 	<nav>
 		<ul>
-			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.path === '/projects'}><a sveltekit:prefetch href="/projects">Projects</a></li>
-			<li class:active={$page.path === '/gallery'}><a sveltekit:prefetch href="/gallery">Gallery</a></li>
-			<li class:active={$page.path === '/'}>
+			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.url.pathname === '/projects'}><a sveltekit:prefetch href="/projects">Projects</a></li>
+			<li class:active={$page.url.pathname === '/gallery'}><a sveltekit:prefetch href="/gallery">Gallery</a></li>
+			<li class:active={$page.url.pathname === '/'}>
 				<a sveltekit:prefetch class="header-logo" href="/">
 					<img src="/logo.svg" alt="MindLabor Logo" width=56px height=56px>
 				</a>
 			</li>
-			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
-			<li class:active={$page.path === '/resume'}><a sveltekit:prefetch href="/resume">Resume</a></li>
-			<li class:active={$page.path === '/contact'}><a sveltekit:prefetch href="/contact">Contact</a></li>
+			<li class:active={$page.url.pathname === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
+			<li class:active={$page.url.pathname === '/resume'}><a sveltekit:prefetch href="/resume">Resume</a></li>
+			<li class:active={$page.url.pathname === '/contact'}><a sveltekit:prefetch href="/contact">Contact</a></li>
 		</ul>
 	</nav>
 </header>
