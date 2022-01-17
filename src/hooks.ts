8f5e5736eef381152a3e1edcf791ece274577f2e
Samuel Braun <42311986+MindLaborDev@users.noreply.github.com>
2022-01-17 17:06:20 +0100

    Minify production html

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,26 +1,52 @@
 import { v4 as uuid } from '@lukeed/uuid';
 import type { Handle } from '@sveltejs/kit';
 import cookie from 'cookie';
+import { minify } from 'html-minifier';
+import { prerendering } from '$app/env';
+
+const minification_options = {
+    collapseBooleanAttributes: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    decodeEntities: true,
+    html5: true,
+    ignoreCustomComments: [/^#/],
+    minifyCSS: true,
+    minifyJS: true,
+    removeAttributeQuotes: true,
+    removeComments: true,
+    removeOptionalTags: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortAttributes: true,
+    sortClassName: true
+};
+
 
 export const handle: Handle = async ({ request, resolve }) => {
-	const cookies = cookie.parse(request.headers.cookie || '');
-	request.locals.userid = cookies.userid || uuid();
+    const cookies = cookie.parse(request.headers.cookie || '');
+    request.locals.userid = cookies.userid || uuid();
 
-	// TODO https://github.com/sveltejs/kit/issues/1046
-	if (request.url.searchParams.has('_method')) {
-		request.method = request.url.searchParams.get('_method').toUpperCase();
-	}
+    // TODO https://github.com/sveltejs/kit/issues/1046
+    if (request.url.searchParams.has('_method')) {
+        request.method = request.url.searchParams.get('_method').toUpperCase();
+    }
 
-	const response = await resolve(request);
+    const response = await resolve(request);
 
-	if (!cookies.userid) {
-		// if this is the first time the user has visited this app,
-		// set a cookie so that we recognise them when they return
-		response.headers['set-cookie'] = cookie.serialize('userid', request.locals.userid, {
-			path: '/',
-			httpOnly: true
-		});
-	}
+    if (!cookies.userid) {
+        // if this is the first time the user has visited this app,
+        // set a cookie so that we recognise them when they return
+        response.headers['set-cookie'] = cookie.serialize('userid', request.locals.userid, {
+            path: '/',
+            httpOnly: true
+        });
+    }
 
-	return response;
+    if (prerendering && response.headers['content-type'] === 'text/html') {
+        response.body = minify(response.body, minification_options);
+    }
+
+    return response;
 };
--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import '../../routes/css/header.css';
+    import { page } from '$app/stores';
+    import '../../routes/css/header.css';
 </script>
 
 <header>
-	<nav>
-		<ul>
-			<li class:active={$page.url.pathname === '/'} class="float-hover"><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.url.pathname === '/projects'} class="float-hover"><a sveltekit:prefetch href="/projects">Projects</a></li>
-			<li class:active={$page.url.pathname === '/gallery'} class="float-hover"><a sveltekit:prefetch href="/gallery">Gallery</a></li>
-			<li class:active={$page.url.pathname === '/'}>
-				<a sveltekit:prefetch class="header-logo" href="/">
-					<img src="/logo.svg" alt="MindLabor Logo" width=56px height=56px>
-				</a>
-			</li>
-			<li class:active={$page.url.pathname === '/about'} class="float-hover"><a sveltekit:prefetch href="/about">About</a></li>
-			<li class:active={$page.url.pathname === '/resume'} class="float-hover"><a sveltekit:prefetch href="/resume">Resume</a></li>
-			<li class:active={$page.url.pathname === '/contact'} class="float-hover"><a sveltekit:prefetch href="/contact">Contact</a></li>
-		</ul>
-	</nav>
+    <nav>
+        <ul>
+            <li class:active={$page.url.pathname === '/'} class="float-hover"><a sveltekit:prefetch href="/">Home</a></li>
+            <li class:active={$page.url.pathname === '/projects'} class="float-hover"><a sveltekit:prefetch href="/projects">Projects</a></li>
+            <li class:active={$page.url.pathname === '/gallery'} class="float-hover"><a sveltekit:prefetch href="/gallery">Gallery</a></li>
+            <li class:active={$page.url.pathname === '/'}>
+                <a sveltekit:prefetch class="header-logo" href="/">
+                    <img src="/logo.svg" alt="MindLabor Logo" width=56px height=56px>
+                </a>
+            </li>
+            <li class:active={$page.url.pathname === '/about'} class="float-hover"><a sveltekit:prefetch href="/about">About</a></li>
+            <li class:active={$page.url.pathname === '/resume'} class="float-hover"><a sveltekit:prefetch href="/resume">Resume</a></li>
+            <li class:active={$page.url.pathname === '/contact'} class="float-hover"><a sveltekit:prefetch href="/contact">Contact</a></li>
+        </ul>
+    </nav>
 </header>
