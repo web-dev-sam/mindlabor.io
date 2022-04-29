d375f1a99c5643211adc871c66906ec845bd1a73
Samuel Braun <office.samigo.a@gmail.com>
2022-04-29 15:28:28 +0200

    Added own header for home

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -3,18 +3,38 @@
     import '../../routes/css/header.css';
 </script>
 
-<header>
-    <nav>
-        <ul>
-            <li class:active={$page.url.pathname === '/'}>
-                <a sveltekit:prefetch class="header-logo" href="/">
-                    <img src="/logo.svg" alt="MindLabor Logo">
-                </a>
-            </li>
-            <li></li>
-            <li class:active={$page.url.pathname === '/'} class="float-hover"><a sveltekit:prefetch href="/">Home</a></li>
-            <li class:active={$page.url.pathname === '/blog'} class="float-hover"><a sveltekit:prefetch href="/blog">Blog</a></li>
-            <li class:active={$page.url.pathname === '/about'} class="float-hover"><a sveltekit:prefetch href="/about">About</a></li>
-        </ul>
-    </nav>
-</header>
+
+{#if $page.url.pathname === '/'}
+    <header class="home-header">
+        <nav>
+            <ul>
+                <li class:active={$page.url.pathname === '/'}>
+                    <a sveltekit:prefetch class="header-logo" href="/">
+                        <img src="/logo.svg" alt="MindLabor Logo">
+                    </a>
+                </li>
+                <li></li>
+                <li class="float-hover"><a sveltekit:prefetch href="/blog">Blog</a></li>
+                <li class="float-hover"><a sveltekit:prefetch href="/about">About</a></li>
+            </ul>
+        </nav>
+    </header>
+{:else}
+    <header class="default-header">
+        <nav>
+            <ul>
+                <li class:active={$page.url.pathname === '/'}>
+                    <a sveltekit:prefetch class="header-logo" href="/">
+                        <img src="/logo.svg" alt="MindLabor Logo">
+                    </a>
+                </li>
+                <li></li>
+                <li class:active={$page.url.pathname === '/'} class="float-hover"><a sveltekit:prefetch href="/">Home</a></li>
+                <li class:active={$page.url.pathname === '/blog'} class="float-hover"><a sveltekit:prefetch href="/blog">Blog</a></li>
+                <li class:active={$page.url.pathname === '/about'} class="float-hover"><a sveltekit:prefetch href="/about">About</a></li>
+            </ul>
+        </nav>
+    </header>
+{/if}
+
+
